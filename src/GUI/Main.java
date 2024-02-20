9c63da029760a2986b97a05390e104967de735cd
MemoLuche <guillermogp08@gmail.com>
2024-02-20 16:27:37 -0600

    Se cambia de color el boton

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -4,7 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        VentanaPrincipal ventanaDos = new VentanaPrincipal();
+        VentanaPrincipal ventanaDos = new VentanaPrincipal(800,800);
 
     }
 
--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -1,9 +1,9 @@
 package GUI;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal extends JFrame{
     
@@ -16,16 +16,31 @@ public class VentanaPrincipal extends JFrame{
         setVisible(true);
         
     }
+    */
     
     public VentanaPrincipal(int x, int y) {
         
         setTitle("GUI 37");
         setSize(x, y);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        
+        setResizable(false); //Evitar que modifiquen el tamaño de la ventana
+        // Crea un panel p1
+        JPanel p1 = new JPanel();
+        add(p1); //Agrega el panel p1 a mi ventana
+        //Especificar el panel del layout
+        p1.setLayout(null);
+        JButton btn2 = new JButton("Cambiar de boton");
+        //Funcion del boton
+        btn2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                cambiarColorBoton(btn2);
+            }
+        });
+        btn2.setBounds(((x*1/2)-40), ((y*1/2)-80), 80, 40); //Coordenadas y tamaño del boton
+        p1.add(btn2); //Agregar el boton al layout
+        setVisible(true); //Permitir visualizar los cambios DEJAR AL FINAL DEL CODIGO
+
     }
-    */
     
     public VentanaPrincipal() {
 
@@ -37,12 +52,26 @@ public class VentanaPrincipal extends JFrame{
         add(p1); //Agrega el panel p1 a mi ventana
         //Especificar el panel del layout
         p1.setLayout(new FlowLayout());
+        //Label
+        JLabel etiqueta = new JLabel("Oprime el botom!");
+        p1.add(etiqueta);
+        //Input
+        JTextField inputT = new JTextField(10);
+        p1.add(inputT);
         //Agregamos botones
         JButton btn1 = new JButton("Click!");
         btn1.setBackground(getForeground());
         p1.add(btn1);
         setVisible(true);
         
+    }
+
+    public void cambiarColorBoton (JButton boton){
+
+        Color nuevoColor = JColorChooser.showDialog(this, "Selecciona un color", boton.getBackground());
+        if(nuevoColor != null){
+            boton.setBackground(nuevoColor);
+        }
     }
 
 }
