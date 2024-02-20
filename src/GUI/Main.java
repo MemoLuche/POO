00d963aacf3f74123b9a7a1113384e8c8f17b904
MemoLuche <guillermogp08@gmail.com>
2024-02-20 15:48:58 -0600

    Agrego un boton

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -4,8 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        VentanaPrincipal ventana = new VentanaPrincipal();
-        VentanaPrincipal ventanaDos = new VentanaPrincipal(250,250, "Hello world!");
+        VentanaPrincipal ventanaDos = new VentanaPrincipal();
 
     }
 
--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -1,34 +1,48 @@
 package GUI;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
 
 public class VentanaPrincipal extends JFrame{
     
+    /* 
     public VentanaPrincipal() {
-
+        
         setTitle("GUI 37");
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
+        
     }
-
+    
     public VentanaPrincipal(int x, int y) {
-
+        
         setTitle("GUI 37");
         setSize(x, y);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
+        
     }
+    */
     
-    public VentanaPrincipal(int x, int y, String titulo) {
+    public VentanaPrincipal() {
 
-        setTitle(titulo);
-        setSize(x, y);
+        setTitle("Hello World!");
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Crea un panel p1
+        JPanel p1 = new JPanel();
+        add(p1); //Agrega el panel p1 a mi ventana
+        //Especificar el panel del layout
+        p1.setLayout(new FlowLayout());
+        //Agregamos botones
+        JButton btn1 = new JButton("Click!");
+        btn1.setBackground(getForeground());
+        p1.add(btn1);
         setVisible(true);
-
+        
     }
 
 }
