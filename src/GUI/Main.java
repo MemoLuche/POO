fc1ad86ab673538a9d89671f1b7d74756279fef2
Guillermo Gordillo <guillermogp08@gmail.com>
2024-02-20 22:00:11 -0600

    Quitar prueba de test

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         
         VentanaPrincipal ventanaDos = new VentanaPrincipal(800,800);
-System.out.println("Esto es una prueba");
 
     }
 
