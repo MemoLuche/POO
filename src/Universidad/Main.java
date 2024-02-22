7bed61b918a511efe7f1cd06a6da188ca3a2fcdb
MemoLuche <guillermogp08@gmail.com>
2024-02-22 02:10:28 -0600

    Se terminaron los metodos para las operaciones solicitadas de las matrices, se agrego un menu para facilitar el uso del programa al usuario final, y se creo consigo una clase para crear dicho menu.

--- src/Universidad/Main.java
+++ src/Universidad/Main.java
@@ -1,5 +1,8 @@
 package Universidad;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void ImprimirMatriz(double[][] matriz) {
@@ -20,30 +23,95 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*
-         * 
-         Alumno a1 = new Alumno(37,"Joel","García","Pérez","SOF","FIF");
-         System.out.println(a1.getExpediente ()); a1.InscPracticas();
-         System.out.println(a1.isReinscrito()); a1.Reinscripcion(9,6); //Modifica el número de materias 
-         System.out.println(a1.isReinscrito());
-         a1.CalifCheckOut(); System.out.println(a1.getPromedioG());
-         System.out.println(a1.getPromedioS()); System.out.println(a1.getSemestre());
-         ImprimirVector(a1.calificaciones); System.out.println(a1.isReinscrito());
-         a1.Reinscripcion(9,4); //Modifica el número de materias
-         System.out.println(a1.isReinscrito()); a1.CalifCheckOut();
-         System.out.println(a1.getPromedioG());
-         System.out.println(a1.getPromedioS());
-         System.out.println(a1.getSemestre());
-         ImprimirVector(a1.calificaciones); System.out.println(a1.isReinscrito());
-         */
-        
+        Scanner sc = new Scanner(System.in);
 
-        // Matriz
-        Matriz m1 = new Matriz("2D", 4, 4);
+        boolean cond = true;
+        int opcion = 0;
+
+        //Crear las matrices a usar
+        System.out.println("Bienvenido a la calculadora de matrices!");
+        System.out.println("Primero debemos crear nuestras dos matrices!");
+        System.out.println("A continuacion sigue los pasos como se indica de favor");
+        System.out.println("Datos de la matriz UNO");
+        Matriz m1 = Menu.crearMatrizUno();
+        System.out.println("Datos de la matriz DOS");
+        Matriz m2 = Menu.crearMatrizDos();
+        System.out.println("Como segundo paso debemos llenar nuestras dos matrices!");
+        System.out.println("A continuacion sigue los pasos como se indica de favor");
+        System.out.println("Datos de la matriz UNO");
         m1.RellenarMatriz();
-        //Matriz m2 = new Matriz("2D2", 2, 2);
-        //m2.RellenarMatriz();
-        System.out.println(m1.Determinante4x4());
-        //ImprimirMatriz(m1.RestaMatrices(m2));
+        System.out.println("Datos de la matriz DOS");
+        m2.RellenarMatriz();
+
+        //Menu de opciones para el usuario
+        do {
+            try {
+                System.out.println("Las opciones son:");
+                System.out.println("1. Reconstruir matriz uno");
+                System.out.println("2. Rellenar matriz uno");
+                System.out.println("3. Reconstruir matriz dos");
+                System.out.println("4. Rellenar matriz dos");
+                System.out.println("5. Mostrar matriz");
+                System.out.println("6. Sumar matrices");
+                System.out.println("7. Restar matrices");
+                System.out.println("8. Determinante de la matriz");
+                System.out.println("9. Multiplicacion de matriz x escalar");
+                System.out.println("10. Multiplicacion de matriz x matriz");
+                System.out.println("11. Transpuesta de matriz");
+                System.out.println("12. Invertir la matriz");
+                System.out.println("13. Cerrar programa");
+                opcion = sc.nextInt();
+                
+            } catch (InputMismatchException e) {
+                System.out.println("Error! Favor de ingresar el unmero entero de la opcion deseada");
+                sc.nextLine();
+            }
+            switch (opcion) {
+                case 1: m1 = Menu.crearMatrizUno();
+                    break;
+            
+                case 2: m1.RellenarMatriz();
+                    break;
+
+                case 3: m2 = Menu.crearMatrizDos();
+                    break;
+                    
+                case 4: m2.RellenarMatriz();
+                    break;
+
+                case 5: Menu.MostrarMatriz(m1, m2);
+                    break;
+
+                case 6:  Menu.SumarMatrices(m1, m2);
+                    break;
+
+                case 7: Menu.RestarMatrices(m1, m2);
+                    break;
+
+                case 8: Menu.DeterminanteUniversal(m1, m2);
+                    break;
+
+                case 9: Menu.MultipicarXEscalar(m1, m2);
+                    break;
+
+                case 10: Menu.MultiplicarMxM(m1, m2);
+                    break;
+
+                case 11: Menu.Transpuesta(m1, m2);
+                    break;
+
+                case 12: Menu.invertirMatriz(m1, m2);
+                    break;
+
+                case 13: System.out.println("Hasta pronto!");
+                cond = false;
+                break;
+
+                default: System.out.println("Ingresar una opcion valida!");
+                break;
+            }
+        } while (cond);
+
+        sc.close();
     }
 }
--- src/Universidad/Matriz.java
+++ src/Universidad/Matriz.java
@@ -10,8 +10,7 @@ public class Matriz {
     double[][] valores; // no definida
 
     // constructor
-    public Matriz(String nombre, int filas, int columnas) {
-        this.nombre = nombre;
+    public Matriz(int filas, int columnas) {
         this.filas = filas;
         this.columnas = columnas;
         this.valores = new double[this.filas][this.columnas];
@@ -27,6 +26,7 @@ public class Matriz {
         }
     }
 
+    //Funcion para llenar las matrices
     public void RellenarMatriz() {
         Scanner sc = new Scanner(System.in);
         for (int f = 0; f < this.valores.length; f++) {
@@ -37,35 +37,42 @@ public class Matriz {
         }
     }
 
-    // sumar Matrices
-    public double[][] SumarMatrices(Matriz m2) {
-        double[][] matRes = this.valores;
+    //Funcion para sumar matrices
+    public Matriz SumarMatrices(Matriz m2) {
         if (this.filas == m2.filas && this.columnas == m2.columnas) {
-            for (int f = 0; f < this.valores.length; f++) {
-                for (int c = 0; c < this.valores[0].length; c++) {
-                    matRes[f][c] += m2.valores[f][c];
+            Matriz resultado = new Matriz(this.filas, this.columnas);
+    
+            for (int f = 0; f < this.filas; f++) {
+                for (int c = 0; c < this.columnas; c++) {
+                    resultado.valores[f][c] = this.valores[f][c] + m2.valores[f][c];
                 }
             }
+            return resultado;
         } else {
             System.out.println("Error!! Dimensiones diferentes!!");
+            return null;  // O maneja el error de alguna otra manera, según tu lógica de programa.
         }
-        return matRes;
-    }
+    }    
 
-    public double[][] RestaMatrices(Matriz m2) {
-        double[][] matRes = this.valores;
+    //Funcion para restar matrices
+    public Matriz RestaMatrices(Matriz m2) {
         if (this.filas == m2.filas && this.columnas == m2.columnas) {
-            for (int f = 0; f < this.valores.length; f++) {
-                for (int c = 0; c < this.valores[0].length; c++) {
-                    matRes[f][c] -= m2.valores[f][c];
+            Matriz resultado = new Matriz(this.filas, this.columnas);
+    
+            for (int f = 0; f < this.filas; f++) {
+                for (int c = 0; c < this.columnas; c++) {
+                    resultado.valores[f][c] = this.valores[f][c] - m2.valores[f][c];
                 }
             }
+            return resultado;
         } else {
             System.out.println("Error!! Dimensiones diferentes!!");
+            return null;  // O maneja el error de alguna otra manera, según tu lógica de programa.
         }
-        return matRes;
     }
+    
 
+    //Funcion para la determinante de 2x2
     public double Determinante() {
         double det = 0.0;
         if (this.filas == 2 && this.columnas == 2) {
@@ -76,6 +83,7 @@ public class Matriz {
         return det;
     }
 
+    //Funcion para la determinante de 3x3
     public double Determinante3x3() {
         if (this.filas == 3 && this.columnas == 3) {
             double det = 0.0;
@@ -91,7 +99,7 @@ public class Matriz {
         }
     }
 
-    // Función para calcular la determinante de una matriz 4x4
+    //Función para calcular la determinante de una matriz 4x4
     public double Determinante4x4() {
         if (this.filas == 4 && this.columnas == 4) {
             double det = 0.0;
@@ -107,15 +115,107 @@ public class Matriz {
         }
     }
 
-    // Función auxiliar para calcular cofactor
+    //Funcion para calcular la multiplicacion de una matrix por un escalar
+    public Matriz MxEscalar(double Escalar) {
+        Matriz matEsc = new Matriz(this.filas, this.columnas);
+        for (int f = 0; f < this.filas; f++) {
+            for (int c = 0; c < this.columnas; c++) {
+                matEsc.valores[f][c] = this.valores[f][c] * Escalar;
+            }
+        }
+    
+        return matEsc;
+    }
+    
+
+    // Función para multiplicar dos matrices
+    public Matriz MultiplicarMxM(Matriz matrizB) {
+        if (this.columnas == matrizB.filas) {
+            Matriz resultado = new Matriz(this.filas, matrizB.columnas);
+
+            for (int i = 0; i < this.filas; i++) {
+                for (int j = 0; j < matrizB.columnas; j++) {
+                    for (int k = 0; k < this.columnas; k++) {
+                        resultado.valores[i][j] += this.valores[i][k] * matrizB.valores[k][j];
+                    }
+                }
+            }
+
+            return resultado;
+        } else {
+            System.out.println("Error!! El número de columnas de la primera matriz debe ser igual al número de filas de la segunda matriz");
+            return null;
+        }
+    }
+
+    //Funcion para caluclar la transpuesta de una matriz
+    public Matriz Transpuesta() {
+        Matriz resultado = new Matriz(this.columnas, this.filas);
+        for (int c = 0; c < this.columnas; c++) {
+            for (int f = 0; f < this.filas; f++) {
+                resultado.valores[c][f] = this.valores[f][c];
+            }
+        }
+        return resultado;
+    }
+
+    //Funcion para calcular la inversa de una matriz por medio del metodo de Gauss
+    public Matriz invertirMatriz() {
+        int n = this.columnas;
+        Matriz aumentada = new Matriz(n, 2*n);
+        //Matriz aumentada [matriz | Identidad]
+        
+        //Inicializar matriz aumentada
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                aumentada.valores[i][j] = this.valores[i][j];
+                
+                //Establecer la parte identidad de la matriz aumentada
+                aumentada.valores[i][j + n] = (i == j) ? 1 : 0;
+            }
+        }
+
+        //Aplicar la eliminación de Gauss-Jordan
+        for (int i = 0; i < n; i++) {
+            //Pivote actual
+            double pivote = aumentada.valores[i][i];
+            
+            //Dividir la fila por el pivote
+            for (int j = 0; j < 2 * n; j++) {
+                aumentada.valores[i][j] /= pivote;
+            }
+
+            //Eliminación hacia abajo y hacia arriba
+            for (int k = 0; k < n; k++) {
+                if (k != i) {
+                    double factor = aumentada.valores[k][i];
+                    for (int j = 0; j < 2 * n; j++) {
+                        aumentada.valores[k][j] -= factor * aumentada.valores[i][j];
+                    }
+                }
+            }
+        }
+
+        //Extraer la parte derecha (la inversa)
+        Matriz inversa = new Matriz(n, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                inversa.valores[i][j] = aumentada.valores[i][j + n];
+            }
+        }
+
+        return inversa;
+    }
+
+    //Función auxiliar para calcular cofactor
     private double cofactor(int fila, int columna) {
         int signo = (fila + columna) % 2 == 0 ? 1 : -1;
         return signo * menor(fila, columna).Determinante3x3();
     }
 
-    // Función auxiliar para obtener la menor de una matriz 3x3
+    //Función auxiliar para obtener la menor de una matriz 3x3
     private Matriz menor(int fila, int columna) {
-        Matriz submatriz = new Matriz("", 3, 3);
+        Matriz submatriz = new Matriz(3, 3);
 
         int subfila = 0;
         int subcolumna = 0;
@@ -136,15 +236,7 @@ public class Matriz {
         return submatriz;
     }
 
-    // Getters/setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
+    //Getters/setters
     public int getFilas() {
         return filas;
     }
